036341af5cfc1198da2b03ddb7a155abece5f11d
Djorkaeff Alexandre Vilela Pereira <djorkaeff.unb@gmail.com>
2018-04-13 14:06:26 -0300

    Update component Release

--- src/container/Release.js
+++ src/container/Release.js
@@ -1,9 +1,61 @@
 import React, { Component } from 'react';
 
 class Release extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            releases: [
+                {
+                    id: 1,
+                    releaseName: 'Release 1',
+                    releaseDate: '01/01/2019',
+                },
+                {
+                    id: 2,
+                    releaseName: 'Release 2',
+                    releaseDate: '01/01/2019',
+                },
+                {
+                    id: 3,
+                    releaseName: 'Release 3',
+                    releaseDate: '01/01/2019',
+                },
+                {
+                    id: 4,
+                    releaseName: 'Release 4',
+                    releaseDate: '01/01/2019',
+                }
+            ]
+        }
+    }
+
     render() {
         return (
-            <h1>Releases</h1>
+            <div className="text-center" style={{marginTop: '25px'}}>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">id</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Date</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.releases.map((release, index) => {
+                            return (
+                                <tr>
+                                    <th scope="row">{release.id}</th>
+                                    <td>{release.releaseName}</td>
+                                    <td>{release.releaseDate}</td>
+                                    <td><button type="button" className="btn btn-outline-danger btn-sm">REMOVER</button></td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }
