00af85fb5a1c18f5b71f699497acf4a63a62db90
Djorkaeff Alexandre Vilela Pereira <djorkaeff.unb@gmail.com>
2018-04-15 14:42:10 -0300

    Update readme.md

--- src/container/Release.js
+++ src/container/Release.js
@@ -12,21 +12,25 @@ class Release extends Component {
                     id: 1,
                     releaseName: 'Release 1',
                     releaseDate: '01/01/2019',
+                    deleted: 0,
                 },
                 {
                     id: 2,
                     releaseName: 'Release 2',
                     releaseDate: '01/01/2019',
+                    deleted: 0,
                 },
                 {
                     id: 3,
                     releaseName: 'Release 3',
                     releaseDate: '01/01/2019',
+                    deleted: 0,
                 },
                 {
                     id: 4,
                     releaseName: 'Release 4',
                     releaseDate: '01/01/2019',
+                    deleted: 0,
                 }
             ]
         }
@@ -39,10 +43,15 @@ class Release extends Component {
             id: state.releases.length +1 ,
             releaseName: name,
             releaseDate: date,
+            deleted: 0,
         }
         this.setState({releases: state.releases.concat(myRelease)});
     }
 
+    removeRelease(id) {
+        alert(id);
+    }
+    
     render() {
         return (
             <div className="text-center" style={{marginTop: '25px'}}>
@@ -58,14 +67,21 @@ class Release extends Component {
                     </thead>
                     <tbody>
                         {this.state.releases.map((release, index) => {
-                            return (
-                                <tr>
-                                    <th scope="row">{release.id}</th>
-                                    <td>{release.releaseName}</td>
-                                    <td>{release.releaseDate}</td>
-                                    <td><button type="button" className="btn btn-danger btn-sm">REMOVER</button></td>
-                                </tr>
-                            );
+                            if(release.deleted === 0 ) {
+                                return (
+                                    <tr>
+                                        <th scope="row">{release.id}</th>
+                                        <td>{release.releaseName}</td>
+                                        <td>{release.releaseDate}</td>
+                                        <td><button type="button" onClick={() => this.removeRelease(release.id)} className="btn btn-danger btn-sm">REMOVER</button></td>
+                                    </tr>
+                                );
+                            }
+                            else {
+                                return (
+                                    <h1>error</h1>
+                                );
+                            }
                         })}
                     </tbody>
                 </table>
